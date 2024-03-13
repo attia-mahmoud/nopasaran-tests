50479dcd59f432e391e3d45642cbb296f126beb1
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-03-13 11:49:29 +0300

    Create create_ca.py

---
+++ create_ca.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Generate a new RSA private key of 4096 bits
+openssl genrsa -out root_ca.key 4096
+
+# Create a self-signed certificate for the root CA
+openssl req -new -x509 -sha256 -key root_ca.key -out root_ca.crt -days 3650 -subj "/C=SA/ST=Jeddah/L=Thuwal/O=NoPASARAN/CN=www.nopasaran.com"
