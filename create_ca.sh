f344c82cfcfbf9ad967f17306e29b531c71cee40
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-03-13 14:08:23 +0300

    Update create_ca.sh

--- create_ca.sh
+++ create_ca.sh
@@ -4,4 +4,4 @@
 openssl genrsa -out root_ca.key 4096
 
 # Create a self-signed certificate for the root CA
-openssl req -new -x509 -sha256 -key root_ca.key -out root_ca.crt -days 3650 -subj "/C=SA/ST=Jeddah/L=Thuwal/O=NoPASARAN/CN=www.nopasaran.com"
+openssl req -new -x509 -sha256 -key root_ca.key -out root_ca.crt -days 3650 -subj "/C=SA/ST=Jeddah/L=Thuwal/O=NoPASARAN"
