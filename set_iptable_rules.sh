2ca1a7e1125fe63b35cfa3eb3715f7c8c8dcb330
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-04-01 02:08:06 +0300

    Create set_iptable_rules.sh

---
+++ set_iptable_rules.sh
@@ -0,0 +1,17 @@
+# Set default chain policies
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Accept on localhost
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# Allow established sessions to receive traffic
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allowing Established Outgoing Connections
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# Allowing Internal Network to access External
+sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
