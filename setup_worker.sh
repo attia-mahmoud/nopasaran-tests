9561f3160ece3984a34545b4eaa2ddca402484fc
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-04-23 19:22:13 +0300

    Update setup_worker.sh

--- setup_worker.sh
+++ setup_worker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check if three arguments were provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <endpoint_number> <destination_ip> <server_port>"
+# Check if four arguments were provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <endpoint_number> <destination_ip> <server_port> <role>"
     exit 1
 fi
 
@@ -13,6 +13,7 @@ NUMBER=$1
 DESTINATION_IP=$2
 SERVER_PORT=$3
 ENDPOINT_NAME="endpoint${NUMBER}"
+ROLE=$4
 
 # Add root_ca.crt to root store
 cp root_ca.crt /usr/local/share/ca-certificates
@@ -40,16 +41,37 @@ cat > "conf_${ENDPOINT_NAME}.json" <<EOF
 }
 EOF
 
-# Create the variables file
-cat > "variables.json" <<EOF
+if [ $4 -e 'client' ]; then
+    # Create the variables file
+    cat > "variables.json" <<EOF
 {
   "ttl": 1,
   "ip_dst": "${DESTINATION_IP}",
   "icmp_filter": "icmp",
   "udp_filter": "udp",
   "role": "client",
+  "quic_command": "python3 examples/http3_client.py https://${DESTINATION_IP}:4433 --insecure",
   "controller_conf_filename": "conf_${ENDPOINT_NAME}.json"
 }
 EOF
+fi
+
+
+if [ $4 -e 'server' ]; then
+    # Create the variables file
+    cat > "variables.json" <<EOF
+{
+  "ttl": 1,
+  "ip_dst": "${DESTINATION_IP}",
+  "icmp_filter": "icmp",
+  "udp_filter": "udp",
+  "role": "server",
+  "quic_command": "python3 examples/http3_server.py --certificate cert.pem --private-key cert.key --crets-log secrets.txt",
+  "controller_conf_filename": "conf_${ENDPOINT_NAME}.json"
+}
+EOF
+fi
+
+
 
 echo "${ENDPOINT_NAME} key, certificate, PEM file, configuration file, and variables file have been created."
