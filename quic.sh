783c1f3b1b59bf3c3fbaba8940fb4550a01b0df0
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-04-23 13:26:24 +0300

    Update quic.sh

--- quic.sh
+++ quic.sh
@@ -8,11 +8,11 @@ apt-get update && apt-get install pip -y
 
 pip install wsproto aioquic starlette jinja2 dnslib
 
-# server
-python examples/http3_server.py --certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem
-
 # Generate a new RSA private key of 4096 bits
 openssl genrsa -out root_ca.key 4096
 
 # Create a self-signed certificate for the root CA
-openssl req -new -x509 -sha256 -key root_ca.key -out root_ca.crt -days 3650 -subj "/"
+openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout cert.key -out cert.pem -config req.cnf -sha256
+
+# server
+python examples/http3_server.py --certificate cert.pem --private-key root_ca.key
