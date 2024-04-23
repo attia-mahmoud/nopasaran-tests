7564e156bd5ebcfdc31dff97e4ef81ffe05c3ca4
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-04-23 20:26:21 +0300

    Update quic.sh

--- quic.sh
+++ quic.sh
@@ -6,7 +6,7 @@
 
 # pip install wsproto aioquic starlette jinja2 dnslib
 
-# # Create a self-signed certificate for the root CA
-# openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout cert.key -out cert.pem -config req.cnf -sha256
+# # Create a self-signed certificate
+openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout cert.key -out cert.pem -config req.cnf -sha256
 
 pip install hypercorn[h3] quart
