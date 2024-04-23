da1a297850fa65eb5c129bb88d1a1adfdeff3a7f
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-04-23 19:37:59 +0300

    Update quic.sh

--- quic.sh
+++ quic.sh
@@ -10,11 +10,3 @@ pip install wsproto aioquic starlette jinja2 dnslib
 
 # Create a self-signed certificate for the root CA
 openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout cert.key -out cert.pem -config req.cnf -sha256
-
-# server
-python3 examples/http3_server.py --certificate cert.pem --private-key cert.key
-
-# client
-python3 examples/http3_client.py https://192.168.122.2:4433 --insecure
-
-# --insecure flag added as it doesn't allow self-signed certs
