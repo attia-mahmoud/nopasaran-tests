d8dac54891a7d25c19869563ab58b7cf3ae43325
Mahmoud Attia <64820897+attia-mahmoud@users.noreply.github.com>
2024-04-23 20:25:51 +0300

    Update quic.sh

--- quic.sh
+++ quic.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/aiortc/aioquic
+# git clone https://github.com/aiortc/aioquic
 
-apt-get update && apt-get install pip -y
+# apt-get update && apt-get install pip -y
 
-pip install wsproto aioquic starlette jinja2 dnslib
+# pip install wsproto aioquic starlette jinja2 dnslib
 
-# Create a self-signed certificate for the root CA
-openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout cert.key -out cert.pem -config req.cnf -sha256
+# # Create a self-signed certificate for the root CA
+# openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout cert.key -out cert.pem -config req.cnf -sha256
+
+pip install hypercorn[h3] quart
